441db698552d339a9f8475c48d5eef510ee9090c
Yash Katariya <yashkatariya@google.com>
2019-04-12 16:52:13 -0700

    Remove contrib
    PiperOrigin-RevId: 243358359

--- lite/examples/object_detection/ios/ObjectDetection/TfliteWrapper/TfliteWrapper.mm
+++ lite/examples/object_detection/ios/ObjectDetection/TfliteWrapper/TfliteWrapper.mm
@@ -22,9 +22,9 @@
 #include <queue>
 
 
-#include "tensorflow/contrib/lite/model.h"
-#include "tensorflow/contrib/lite/kernels/register.h"
-#include "tensorflow/contrib/lite/string_util.h"
+#include "tensorflow/lite/model.h"
+#include "tensorflow/lite/kernels/register.h"
+#include "tensorflow/lite/string_util.h"
 
 NSString* modelFileName;
 
