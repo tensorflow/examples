711b11dc4b38b76d3995610dfffbca52611f60dc
Khanh LeViet <khanhlvg@google.com>
2019-09-20 10:09:33 -0700

    Add missing files in TF Lite Digit Classifier Android
    PiperOrigin-RevId: 270294985

---
+++ lite/examples/digit_classifier/android/app/src/main/java/org/tensorflow/lite/examples/digitclassifier/MainActivity.kt
@@ -0,0 +1,84 @@
+package org.tensorflow.lite.examples.digitclassifier
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.divyanshu.draw.widget.DrawView
+
+class MainActivity : AppCompatActivity() {
+
+  private var drawView: DrawView? = null
+  private var clearButton: Button? = null
+  private var predictedTextView: TextView? = null
+  private var digitClassifier = DigitClassifier(this)
+
+  @SuppressLint("ClickableViewAccessibility")
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    // Setup view instances
+    drawView = findViewById(R.id.draw_view)
+    drawView?.setStrokeWidth(150.0f)
+    drawView?.setColor(Color.WHITE)
+    drawView?.setBackgroundColor(Color.BLACK)
+    clearButton = findViewById(R.id.clear_button)
+    predictedTextView = findViewById(R.id.predicted_text)
+
+    // Setup clear drawing button
+    clearButton?.setOnClickListener {
+      drawView?.clearCanvas()
+      predictedTextView?.text = getString(R.string.prediction_text_placeholder)
+    }
+
+    // Setup classification trigger so that it classify after every stroke drew
+    drawView?.setOnTouchListener { _, event ->
+      // As we have interrupted DrawView's touch event,
+      // we first need to pass touch events through to the instance for the drawing to show up
+      drawView?.onTouchEvent(event)
+
+      // Then if user finished a touch event, run classification
+      if (event.action == MotionEvent.ACTION_UP) {
+        classifyDrawing()
+      }
+
+      true
+    }
+
+    // Setup digit classifier
+    digitClassifier
+      .initialize()
+      .addOnFailureListener { e -> Log.e(TAG, "Error to setting up digit classifier.", e) }
+  }
+
+  override fun onDestroy() {
+    digitClassifier.close()
+    super.onDestroy()
+  }
+
+  private fun classifyDrawing() {
+    val bitmap = drawView?.getBitmap()
+
+    if ((bitmap != null) && (digitClassifier.isInitialized)) {
+      digitClassifier
+        .classifyAsync(bitmap)
+        .addOnSuccessListener { resultText -> predictedTextView?.text = resultText }
+        .addOnFailureListener { e ->
+          predictedTextView?.text = getString(
+            R.string.classification_error_message,
+            e.localizedMessage
+          )
+          Log.e(TAG, "Error classifying drawing.", e)
+        }
+    }
+  }
+
+  companion object {
+    private const val TAG = "MainActivity"
+  }
+}
