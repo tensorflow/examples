e7a2225d3c0ef319f66f868ceab6f1a09122a9c1
Khanh LeViet <khanhlvg@google.com>
2019-09-30 20:06:02 -0700

    Add TF Lite digit classifier codelab
    PiperOrigin-RevId: 272122709

---
+++ lite/codelabs/digit_classifier/android/finish/app/src/main/java/org/tensorflow/lite/codelabs/digitclassifier/MainActivity.kt
@@ -0,0 +1,99 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+package org.tensorflow.lite.codelabs.digitclassifier
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.divyanshu.draw.widget.DrawView
+
+class MainActivity : AppCompatActivity() {
+
+  private var drawView: DrawView? = null
+  private var clearButton: Button? = null
+  private var predictedTextView: TextView? = null
+  private var digitClassifier = DigitClassifier(this)
+
+  @SuppressLint("ClickableViewAccessibility")
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    // Setup view instances.
+    drawView = findViewById(R.id.draw_view)
+    drawView?.setStrokeWidth(150.0f)
+    drawView?.setColor(Color.WHITE)
+    drawView?.setBackgroundColor(Color.BLACK)
+    clearButton = findViewById(R.id.clear_button)
+    predictedTextView = findViewById(R.id.predicted_text)
+
+    // Setup clear drawing button.
+    clearButton?.setOnClickListener {
+      drawView?.clearCanvas()
+      predictedTextView?.text = getString(R.string.prediction_text_placeholder)
+    }
+
+    // Setup classification trigger so that it classify after every stroke drew.
+    drawView?.setOnTouchListener { _, event ->
+      // As we have interrupted DrawView's touch event,
+      // we first need to pass touch events through to the instance for the drawing to show up.
+      drawView?.onTouchEvent(event)
+
+      // Then if user finished a touch event, run classification
+      if (event.action == MotionEvent.ACTION_UP) {
+        classifyDrawing()
+      }
+
+      true
+    }
+
+    // Setup digit classifier.
+    digitClassifier
+      .initialize()
+      .addOnFailureListener { e -> Log.e(TAG, "Error to setting up digit classifier.", e) }
+  }
+
+  override fun onDestroy() {
+    // Sync DigitClassifier instance lifecycle with MainActivity lifecycle,
+    // and free up resources (e.g. TF Lite instance) once the activity is destroyed.
+    digitClassifier.close()
+    super.onDestroy()
+  }
+
+  private fun classifyDrawing() {
+    val bitmap = drawView?.getBitmap()
+
+    if ((bitmap != null) && (digitClassifier.isInitialized)) {
+      digitClassifier
+        .classifyAsync(bitmap)
+        .addOnSuccessListener { resultText -> predictedTextView?.text = resultText }
+        .addOnFailureListener { e ->
+          predictedTextView?.text = getString(
+            R.string.classification_error_message,
+            e.localizedMessage
+          )
+          Log.e(TAG, "Error classifying drawing.", e)
+        }
+    }
+  }
+
+  companion object {
+    private const val TAG = "MainActivity"
+  }
+}
