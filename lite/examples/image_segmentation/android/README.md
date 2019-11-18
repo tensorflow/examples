85dc67c59f3f56d96e112b8594683a80141a99bd
Mehmet Gungoren <mehmetgungoren@lyrebirdstudio.net>
2019-11-18 13:44:02 +0300

    implementation of take picture and picture selection from device update readme

--- lite/examples/image_segmentation/android/README.md
+++ lite/examples/image_segmentation/android/README.md
@@ -1,1 +1,93 @@
-# Image Segmentation Android App (Coming Soon)
+# Image Segmentation Android App
+
+## Overview
+
+This is an example application for [TensorFlow Lite](https://tensorflow.org/lite)
+on Android. It uses
+[Image segmentation](https://www.tensorflow.org/lite/models/segmentation/overview)
+to segment a demo [*[Pixabay](https://pixabay.com/photos/cap-boy-smile-tomboy-emotions-2923682/).*] picture, picture selection from device and camera.
+Inference is performed using the TensorFlow Lite Java API.
+
+These instructions walk you through building and
+running the demo on an Android device. For an explanation of the source, see
+[TensorFlow Lite Android image segmentation example](https://www.tensorflow.org/lite/models/segmentation/overview).
+
+<!-- TODO(b/124116863): Add app screenshot. -->
+
+### Model
+For details of the model used, visit [Image segmentation](https://www.tensorflow.org/lite/models/segmentation/overview).
+
+Downloading, extracting, and placing the model in the assets folder is managed
+automatically by download.gradle.
+
+## Requirements
+
+*   Android Studio 3.2 (installed on a Linux, Mac or Windows machine)
+
+*   Android device in
+    [developer mode](https://developer.android.com/studio/debug/dev-options)
+    with USB debugging enabled
+
+*   USB cable (to connect Android device to your computer)
+
+## Build and run
+
+### Step 1. Clone the TensorFlow examples source code
+
+Clone the TensorFlow examples GitHub repository to your computer to get the demo
+application.
+
+```
+git clone https://github.com/tensorflow/examples
+```
+
+Open the TensorFlow source code in Android Studio. To do this, open Android
+Studio and select `Open an existing project`, setting the folder to
+`examples/lite/examples/image_segmentation/android`
+
+<img src="images/segment_img1.png?raw=true" />
+
+### Step 2. Build the Android Studio project
+
+Select `Build -> Make Project` and check that the project builds successfully.
+You will need Android SDK configured in the settings. You'll need at least SDK
+version 23. The `build.gradle` file will prompt you to download any missing
+libraries.
+
+The file `download.gradle` directs gradle to download the two models used in the
+example, placing them into `assets`.
+
+<img src="images/segment_img4.png?raw=true" style="width: 40%" />
+
+<img src="images/segment_img2.png?raw=true" style="width: 60%" />
+
+<aside class="note"><b>Note:</b><p>`build.gradle` is configured to use
+TensorFlow Lite's nightly build.</p><p>If you see a build error related to
+compatibility with Tensorflow Lite's Java API (for example, `method X is
+undefined for type Interpreter`), there has likely been a backwards compatible
+change to the API. You will need to run `git pull` in the examples repo to
+obtain a version that is compatible with the nightly build.</p></aside>
+
+### Step 3. Install and run the app
+
+Connect the Android device to the computer and be sure to approve any ADB
+permission prompts that appear on your phone. Select `Run -> Run app.` Select
+the deployment target in the connected devices to the device on which the app
+will be installed. This will install the app on the device.
+
+<img src="images/segment_img5.png?raw=true" style="width: 60%" />
+
+<img src="images/segment_img6.png?raw=true" style="width: 70%" />
+
+<img src="images/segment_img7.png?raw=true" style="width: 40%" />
+
+<img src="images/segment_img8.png?raw=true" style="width: 80%" />
+
+To test the app, open the app called `TFL Classify` on your device. When you run
+the app the first time, the app will request permission to access the camera.
+Re-installing the app may require you to uninstall the previous installations.
+
+## Assets folder
+_Do not delete the assets folder content_. If you explicitly deleted the
+files, choose `Build -> Rebuild` to re-download the deleted model files into the
+assets folder.
--- lite/examples/image_segmentation/android/app/src/main/java/org/tensorflow/lite/examples/segmentation/SegmentationActivity.kt
+++ lite/examples/image_segmentation/android/app/src/main/java/org/tensorflow/lite/examples/segmentation/SegmentationActivity.kt
@@ -1,26 +1,38 @@
 package org.tensorflow.lite.examples.segmentation
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
-
-import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
-import org.tensorflow.lite.examples.segmentation.ext.cropCenter
-
-import org.tensorflow.lite.examples.segmentation.tflite.ImageSegmentator
-import org.tensorflow.lite.examples.segmentation.tflite.SegmentationResult
-import android.text.Spannable
-import android.text.style.ForegroundColorSpan
 import android.graphics.Color
-import android.os.Handler
-import android.os.HandlerThread
+import android.net.Uri
+import android.os.*
+import android.provider.MediaStore
+import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.BackgroundColorSpan
+import android.text.style.ForegroundColorSpan
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.exifinterface.media.ExifInterface
 import com.addisonelliott.segmentedbutton.SegmentedButtonGroup
 import org.tensorflow.lite.examples.segmentation.env.Logger
+import org.tensorflow.lite.examples.segmentation.ext.cropCenter
+import org.tensorflow.lite.examples.segmentation.ext.transformUp
+import org.tensorflow.lite.examples.segmentation.tflite.ImageSegmentator
+import org.tensorflow.lite.examples.segmentation.tflite.SegmentationResult
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class SegmentationActivity: AppCompatActivity() {
 
@@ -50,6 +62,20 @@ class SegmentationActivity: AppCompatActivity() {
     /// Image segmentation result.
     private var segmentationResult: SegmentationResult? = null
 
+    private var pictureImagePath:String? = null
+
+    companion object {
+        val SDF = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH)
+
+        const val STORAGE_PERMISSION_REQUEST_CODE = 101
+        const val CAMERA_PERMISSION_REQUEST_CODE = 102
+
+        const val CHOOSE_PHOTO_ACTIVITY_REQUEST_CODE = 501
+        const val TAKE_PHOTO_ACTIVITY_REQUEST_CODE = 502
+
+        const val ONE_SECOND_IN_MILLIS = 1000L
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,9 +101,59 @@ class SegmentationActivity: AppCompatActivity() {
 
         imageSegmentator = ImageSegmentator(this)
 
+        cameraImageView.setOnClickListener {
+            openCamera()
+        }
+
+        photoImageView.setOnClickListener {
+            openGallery()
+        }
+
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            cameraImageView.alpha = 1.0f
+            cameraImageView.isEnabled = true
+        } else {
+            cameraImageView.alpha = 0.5f
+            cameraImageView.isEnabled = false
+        }
+
         Handler().postDelayed({
             runSegmentation(BitmapFactory.decodeResource(resources, R.drawable.boy))
-        }, 1000)
+        }, ONE_SECOND_IN_MILLIS)
+    }
+
+    private fun openGallery() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), STORAGE_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), CHOOSE_PHOTO_ACTIVITY_REQUEST_CODE)
+    }
+
+    private fun openCamera() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), CAMERA_PERMISSION_REQUEST_CODE)
+            return
+        }
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            val timeStamp = SDF.format(Date())
+            val imageFileName = "$timeStamp.jpg"
+            val storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            pictureImagePath = storageDir.absolutePath + "/" + imageFileName
+            val file = File(pictureImagePath)
+            val outputFileUri = Uri.fromFile(file)
+            val builder = StrictMode.VmPolicy.Builder()
+            StrictMode.setVmPolicy(builder.build())
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri)
+            startActivityForResult(takePictureIntent, TAKE_PHOTO_ACTIVITY_REQUEST_CODE)
+        }
     }
 
     @Synchronized
@@ -231,5 +307,99 @@ Visualization: ${segmentationResult.visualizationTime}ms."""
         legendLabel.text = ""
         segmentedControl.isEnabled = false
         segmentedControl.setPosition(0, true)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (permissions.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                openCamera()
+                return
+            }
+        } else if (requestCode == STORAGE_PERMISSION_REQUEST_CODE) {
+            if (permissions.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery()
+                return
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == TAKE_PHOTO_ACTIVITY_REQUEST_CODE) {
+            if (resultCode != Activity.RESULT_OK) {
+               return
+            }
+            val takenPhoto = File(pictureImagePath)
+            if (takenPhoto.exists()) {
+                val bitmap = readBitmapFromStorage(Uri.fromFile(takenPhoto))!!
+                Handler().postDelayed({
+                    runSegmentation(bitmap)
+                }, ONE_SECOND_IN_MILLIS)
+            }
+            return
+        } else if (requestCode == CHOOSE_PHOTO_ACTIVITY_REQUEST_CODE) {
+            if (resultCode != Activity.RESULT_OK) {
+                return
+            }
+            val bitmap = readBitmapFromStorage(data?.data!!)!!
+            Handler().postDelayed({
+                runSegmentation(bitmap)
+            }, ONE_SECOND_IN_MILLIS)
+            return
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun readBitmapFromStorage(uri: Uri): Bitmap? {
+        var options = BitmapFactory.Options().apply {
+            inJustDecodeBounds = true
+        }
+        BitmapFactory.decodeStream(contentResolver.openInputStream(uri), null, options)
+        val sampleSize = calculateInSampleSize(options, 1000, 1000)
+        options = BitmapFactory.Options().apply {
+            inJustDecodeBounds = false
+            inSampleSize = sampleSize
+        }
+        val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(uri), null, options)
+        return bitmap?.transformUp(getOrientation(contentResolver.openInputStream(uri)))
+    }
+
+    private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+        // Raw height and width of image
+        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
+
+        if (height > reqHeight || width > reqWidth) {
+
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+
+        return inSampleSize
+    }
+
+    private fun getOrientation(inputStream: InputStream?): Int {
+        val exifInterface: ExifInterface
+        var orientation = 0
+        try {
+            exifInterface = ExifInterface(inputStream!!)
+            orientation = exifInterface.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED
+            )
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return orientation
     }
 }
