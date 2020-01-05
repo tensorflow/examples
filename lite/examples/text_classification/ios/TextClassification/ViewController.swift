dcd7ada6f013724cdd4996f4d8ab0daa527861a8
Khurram Shehzad <khurram.pak522@gmail.com>
2020-01-06 00:22:50 +0500

    Add TextClassification iOS Project

---
+++ lite/examples/text_classification/ios/TextClassification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextClassification
+//
+//  Created by Khurram Shehzad on 06/01/2020.
+//  Copyright © 2020 Khurram Shehzad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lite/examples/text_classification/ios/TextClassificationUITests/TextClassificationUITests.swift
@@ -0,0 +1,43 @@
+//
+//  TextClassificationUITests.swift
+//  TextClassificationUITests
+//
+//  Created by Khurram Shehzad on 06/01/2020.
+//  Copyright © 2020 Khurram Shehzad. All rights reserved.
+//
+
+import XCTest
+
+class TextClassificationUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
