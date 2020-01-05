275426982dd55348c06d84cfaacb433b3d4575ea
Khurram Shehzad <khurram.pak522@gmail.com>
2020-01-06 00:32:10 +0500

    Remove extra code

--- lite/examples/text_classification/ios/TextClassification/ViewController.swift
+++ lite/examples/text_classification/ios/TextClassification/ViewController.swift
@@ -9,12 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+  
+override func viewDidLoad() {
+  super.viewDidLoad()
+  // Do any additional setup after loading the view.
 }
-
+  
+} // class ViewController
