532ac6ddfc4655092d3e9e15a2f576d920ae7398
Khurram Shehzad <khurram.pak522@gmail.com>
2020-01-13 11:36:01 +0500

    Implement view controller

--- lite/examples/text_classification/ios/TextClassification/ViewController.swift
+++ lite/examples/text_classification/ios/TextClassification/ViewController.swift
@@ -12,8 +12,17 @@ class ViewController: UIViewController {
   
 private var textClassificationClient: TextClassificationnClient?
   
+@IBOutlet weak var scrollView: UIScrollView!
+@IBOutlet weak var textView: UITextView!
+@IBOutlet weak var textField: UITextField!
+  
+@IBAction func onPredictTap(_ sender: Any) {
+  guard let text = textField.text else { return }
+  classify(text)
+}
 override func viewDidLoad() {
   super.viewDidLoad()
+  addKeyboardNotifications()
   DispatchQueue.global().async {
     self.loadClient()
   }
@@ -24,3 +33,54 @@ private func loadClient() {
 }
   
 } // class ViewController
+
+// Keybaord notifications
+extension ViewController {
+private func addKeyboardNotifications() {
+  NotificationCenter.default.addObserver(self,
+                                         selector: #selector(keyboardWillShow(_:)),
+                                         name: UIResponder.keyboardWillShowNotification,
+                                         object: nil)
+  
+  NotificationCenter.default.addObserver(self,
+                                         selector: #selector(keyboardWillHide(_:)),
+                                         name: UIResponder.keyboardWillHideNotification,
+                                         object: nil)
+}
+@objc private func keyboardWillShow(_ notification: Notification) {
+  adjustInsetForKeyboardShow(true, notification: notification)
+}
+
+@objc private func keyboardWillHide(_ notification: Notification) {
+  adjustInsetForKeyboardShow(false, notification: notification)
+}
+private func adjustInsetForKeyboardShow(_ show: Bool, notification: Notification) {
+  guard
+    let userInfo = notification.userInfo,
+    let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+    return
+  }
+  let adjustmentHeight = (keyboardFrame.cgRectValue.height + 20) * (show ? 1 : -1)
+  scrollView.contentInset.bottom += adjustmentHeight
+  scrollView.verticalScrollIndicatorInsets.bottom += adjustmentHeight
+}
+} // extension ViewController
+
+extension ViewController {
+private func classify(_ text: String) {
+  DispatchQueue.global().async {
+    guard let results = self.textClassificationClient?.classify(text: text) else { return }
+    self.showResults(inputText: text, results: results)
+  }
+}
+private func showResults(inputText: String, results: [Result]) {
+  DispatchQueue.main.async {
+    var textToShow = "Input: " + inputText + "\nOutput:\n";
+    for result in results {
+      textToShow.append(result.title + " " + String(result.confidence) + "\n")
+    }
+    textToShow += "---------\n"
+    self.textView.text += textToShow
+  }
+}
+} // extension ViewController
