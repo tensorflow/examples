6e52930362874f135ba2e1056264afa590a7d16d
Daisuke TONOSAKI <daisuke.t.jp@gmail.com>
2020-05-11 20:42:47 +0900

    Remove unnecessary allocateTensors()

--- lite/examples/digit_classifier/ios/DigitClassifier/DigitClassifier.swift
+++ lite/examples/digit_classifier/ios/DigitClassifier/DigitClassifier.swift
@@ -99,10 +99,7 @@ class DigitClassifier {
             print("Failed to convert the image buffer to RGB data.")
             return
         }
-
-        // Allocate memory for the model's input `Tensor`s.
-        try self.interpreter.allocateTensors()
-
+        
         // Copy the RGB data to the input `Tensor`.
         try self.interpreter.copy(rgbData, toInputAt: 0)
 
