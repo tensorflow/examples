faaf7ba789bfc74736bd303fc4f04e45088fba93
Yuqi Li <yuqili@google.com>
2020-07-30 02:28:19 -0700

    Add common module in __init__.py under model_maker to simplify the import path.
    PiperOrigin-RevId: 323964119

--- tensorflow_examples/lite/model_maker/__init__.py
+++ tensorflow_examples/lite/model_maker/__init__.py
@@ -11,3 +11,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""TFLite Model Maker library."""
+
+from tensorflow_examples.lite.model_maker.core.data_util import image_dataloader
+from tensorflow_examples.lite.model_maker.core.data_util import text_dataloader
+from tensorflow_examples.lite.model_maker.core.data_util.image_dataloader import ImageClassifierDataLoader
+from tensorflow_examples.lite.model_maker.core.data_util.text_dataloader import QuestionAnswerDataLoader
+from tensorflow_examples.lite.model_maker.core.data_util.text_dataloader import TextClassifierDataLoader
+
+from tensorflow_examples.lite.model_maker.core.task import configs
+from tensorflow_examples.lite.model_maker.core.task import image_classifier
+from tensorflow_examples.lite.model_maker.core.task import model_spec
+from tensorflow_examples.lite.model_maker.core.task import question_answer
+from tensorflow_examples.lite.model_maker.core.task import text_classifier
