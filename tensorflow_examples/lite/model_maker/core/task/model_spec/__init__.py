9c795bcd8520dc726c87cd699e714f1fbc0226dd
Tiezhen WANG <wangtz@google.com>
2020-12-03 06:44:43 -0800

    Model Maker: Add AudioSpec to the list of model specs.
    PiperOrigin-RevId: 345439955

--- tensorflow_examples/lite/model_maker/core/task/model_spec/__init__.py
+++ tensorflow_examples/lite/model_maker/core/task/model_spec/__init__.py
@@ -15,6 +15,7 @@
 
 import inspect
 
+from tensorflow_examples.lite.model_maker.core.task.model_spec import audio_spec
 from tensorflow_examples.lite.model_maker.core.task.model_spec.image_spec import efficientnet_lite0_spec
 from tensorflow_examples.lite.model_maker.core.task.model_spec.image_spec import efficientnet_lite1_spec
 from tensorflow_examples.lite.model_maker.core.task.model_spec.image_spec import efficientnet_lite2_spec
@@ -23,7 +24,6 @@ from tensorflow_examples.lite.model_maker.core.task.model_spec.image_spec import
 from tensorflow_examples.lite.model_maker.core.task.model_spec.image_spec import ImageModelSpec
 from tensorflow_examples.lite.model_maker.core.task.model_spec.image_spec import mobilenet_v2_spec
 from tensorflow_examples.lite.model_maker.core.task.model_spec.image_spec import resnet_50_spec
-
 from tensorflow_examples.lite.model_maker.core.task.model_spec.text_spec import average_word_vec_spec
 from tensorflow_examples.lite.model_maker.core.task.model_spec.text_spec import AverageWordVecModelSpec
 from tensorflow_examples.lite.model_maker.core.task.model_spec.text_spec import bert_classifier_spec
@@ -36,8 +36,10 @@ from tensorflow_examples.lite.model_maker.core.task.model_spec.text_spec import 
 from tensorflow_examples.lite.model_maker.core.task.model_spec.text_spec import mobilebert_qa_spec
 from tensorflow_examples.lite.model_maker.core.task.model_spec.text_spec import mobilebert_qa_squad_spec
 
+
 # A dict for model specs to make it accessible by string key.
 MODEL_SPECS = {
+    # Image classification
     'efficientnet_lite0': efficientnet_lite0_spec,
     'efficientnet_lite1': efficientnet_lite1_spec,
     'efficientnet_lite2': efficientnet_lite2_spec,
@@ -45,13 +47,20 @@ MODEL_SPECS = {
     'efficientnet_lite4': efficientnet_lite4_spec,
     'mobilenet_v2': mobilenet_v2_spec,
     'resnet_50': resnet_50_spec,
+
+    # Text classification
     'average_word_vec': average_word_vec_spec,
     'bert': bert_spec,
     'bert_classifier': bert_classifier_spec,
+
+    # Question answering
     'bert_qa': bert_qa_spec,
     'mobilebert_classifier': mobilebert_classifier_spec,
     'mobilebert_qa': mobilebert_qa_spec,
     'mobilebert_qa_squad': mobilebert_qa_squad_spec,
+
+    # Audio classification
+    'audio_browser_fft': audio_spec.BrowserFFTSpec,
 }
 
 # List constants for supported models.
@@ -63,6 +72,7 @@ TEXT_CLASSIFICATION_MODELS = [
     'bert_classifier', 'average_word_vec', 'mobilebert_classifier'
 ]
 QUESTION_ANSWERING_MODELS = ['bert_qa', 'mobilebert_qa', 'mobilebert_qa_squad']
+AUDIO_CLASSIFICATION_MODELS = ['audio_browser_fft']
 
 
 def get(spec_or_str):
