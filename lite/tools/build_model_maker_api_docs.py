ce6f2a30c99e36633a70b448ed3159b0819add0c
Mark Daoust <markdaoust@google.com>
2021-03-04 18:04:02 -0800

    Fix doc generation for tflite_model_maker
    PiperOrigin-RevId: 361045293

--- lite/tools/build_model_maker_api_docs.py
+++ lite/tools/build_model_maker_api_docs.py
@@ -57,7 +57,8 @@ flags.DEFINE_string('code_url_prefix',
 flags.DEFINE_bool('search_hints', True,
                   'Include metadata search hints in the generated files')
 
-flags.DEFINE_string('site_path', '/', 'Path prefix in the _toc.yaml')
+flags.DEFINE_string('site_path', '/lite/api_docs/python',
+                    'Path prefix in the _toc.yaml')
 
 FLAGS = flags.FLAGS
 
