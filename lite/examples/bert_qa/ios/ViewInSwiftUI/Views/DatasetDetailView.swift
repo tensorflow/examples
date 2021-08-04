6ff12f37ae328a481591b692f50ca36725c36d92
Sunit Roy <2703iamsry@gmail.com>
2021-08-05 03:16:57 +0530

    SwiftUI Implementation & Code Clean-up

--- lite/examples/bert_qa/ios/ViewInSwiftUI/Views/DatasetDetailView.swift
+++ lite/examples/bert_qa/ios/ViewInSwiftUI/Views/DatasetDetailView.swift
@@ -104,7 +104,10 @@ struct DatasetDetailView: View {
     statusMessage = result.description
 
     // Render the answer in the `contentView`.
-    highlightRange = result.answer.text.range
+    //highlightRange = result.answer.text.range
+    if let answerRange = dataset.content.range(of: result.answer.text){
+    highlightRange = answerRange
+    }
   }
 
   func hideKeyboard() {
