e11ec95fb5cc166c37042b961b64600570b25550
doyoung-gwak <tucan9389@gmail.com>
2021-09-13 19:59:24 +0900

    Replace pure TensorFlowLiteSwift to TFLiteSwift-Vision for simplifying vision preprocessing

--- lite/examples/image_classification/ios/ImageClassification/ModelDataHandler/ModelDataHandler.swift
+++ lite/examples/image_classification/ios/ImageClassification/ModelDataHandler/ModelDataHandler.swift
@@ -12,10 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import CoreImage
-import TensorFlowLite
-import UIKit
-import Accelerate
+import TFLiteSwift_Vision
 
 /// A result from invoking the `Interpreter`.
 struct Result {
@@ -44,7 +41,6 @@ enum MobileNet {
 class ModelDataHandler {
 
   // MARK: - Internal Properties
-
   /// The current thread count used by the TensorFlow Lite Interpreter.
   let threadCount: Int
 
@@ -52,48 +48,36 @@ class ModelDataHandler {
   let threadCountLimit = 10
 
   // MARK: - Model Parameters
-
-  let batchSize = 1
-  let inputChannels = 3
-  let inputWidth = 224
-  let inputHeight = 224
+  var inputWidth: Int { interpreter.inputWidth ?? -1 }
+  var inputHeight: Int { interpreter.inputHeight ?? -1 }
 
   // MARK: - Private Properties
-
   /// List of labels from the given labels file.
   private var labels: [String] = []
 
   /// TensorFlow Lite `Interpreter` object for performing inference on a given model.
-  private var interpreter: Interpreter
+  private var interpreter: TFLiteVisionInterpreter
 
   /// Information about the alpha component in RGBA data.
   private let alphaComponent = (baseOffset: 4, moduloRemainder: 3)
 
   // MARK: - Initialization
-
   /// A failable initializer for `ModelDataHandler`. A new instance is created if the model and
   /// labels files are successfully loaded from the app's main bundle. Default `threadCount` is 1.
   init?(modelFileInfo: FileInfo, labelsFileInfo: FileInfo, threadCount: Int = 1) {
     let modelFilename = modelFileInfo.name
 
-    // Construct the path to the model file.
-    guard let modelPath = Bundle.main.path(
-      forResource: modelFilename,
-      ofType: modelFileInfo.extension
-    ) else {
-      print("Failed to load the model file with name: \(modelFilename).")
-      return nil
-    }
-
     // Specify the options for the `Interpreter`.
     self.threadCount = threadCount
-    var options = InterpreterOptions()
-    options.threadCount = threadCount
+    let options = TFLiteVisionInterpreter.Options(
+      modelName: modelFilename,
+      threadCount: threadCount,
+      normalization: .scaled(from: 0.0, to: 1.0)
+    )
+
     do {
       // Create the `Interpreter`.
-      interpreter = try Interpreter(modelPath: modelPath, options: options)
-      // Allocate memory for the model's input `Tensor`s.
-      try interpreter.allocateTensors()
+      interpreter = try TFLiteVisionInterpreter(options: options)
     } catch let error {
       print("Failed to create the interpreter with error: \(error.localizedDescription)")
       return nil
@@ -103,72 +87,25 @@ class ModelDataHandler {
   }
 
   // MARK: - Internal Methods
-
   /// Performs image preprocessing, invokes the `Interpreter`, and processes the inference results.
   func runModel(onFrame pixelBuffer: CVPixelBuffer) -> Result? {
+
+    // Run inference by invoking the `Interpreter`.
     
-    let sourcePixelFormat = CVPixelBufferGetPixelFormatType(pixelBuffer)
-    assert(sourcePixelFormat == kCVPixelFormatType_32ARGB ||
-             sourcePixelFormat == kCVPixelFormatType_32BGRA ||
-               sourcePixelFormat == kCVPixelFormatType_32RGBA)
-
-
-    let imageChannels = 4
-    assert(imageChannels >= inputChannels)
-
-    // Crops the image to the biggest square in the center and scales it down to model dimensions.
-    let scaledSize = CGSize(width: inputWidth, height: inputHeight)
-    guard let thumbnailPixelBuffer = pixelBuffer.centerThumbnail(ofSize: scaledSize) else {
-      return nil
-    }
-
     let interval: TimeInterval
-    let outputTensor: Tensor
+    let output: TFLiteFlatArray<Float32>?
     do {
-      let inputTensor = try interpreter.input(at: 0)
-
-      // Remove the alpha component from the image buffer to get the RGB data.
-      guard let rgbData = rgbDataFromBuffer(
-        thumbnailPixelBuffer,
-        byteCount: batchSize * inputWidth * inputHeight * inputChannels,
-        isModelQuantized: inputTensor.dataType == .uInt8
-      ) else {
-        print("Failed to convert the image buffer to RGB data.")
-        return nil
-      }
-
-      // Copy the RGB data to the input `Tensor`.
-      try interpreter.copy(rgbData, toInputAt: 0)
-
-      // Run inference by invoking the `Interpreter`.
       let startDate = Date()
-      try interpreter.invoke()
+      // Get the output `TFLiteFlatArray` to process the inference results.
+      output = try interpreter.inference(with: pixelBuffer).first
+      
       interval = Date().timeIntervalSince(startDate) * 1000
-
-      // Get the output `Tensor` to process the inference results.
-      outputTensor = try interpreter.output(at: 0)
     } catch let error {
       print("Failed to invoke the interpreter with error: \(error.localizedDescription)")
       return nil
     }
-
-    let results: [Float]
-    switch outputTensor.dataType {
-    case .uInt8:
-      guard let quantization = outputTensor.quantizationParameters else {
-        print("No results returned because the quantization values for the output tensor are nil.")
-        return nil
-      }
-      let quantizedResults = [UInt8](outputTensor.data)
-      results = quantizedResults.map {
-        quantization.scale * Float(Int($0) - quantization.zeroPoint)
-      }
-    case .float32:
-      results = [Float32](unsafeData: outputTensor.data) ?? []
-    default:
-      print("Output tensor data type \(outputTensor.dataType) is unsupported for this example app.")
-      return nil
-    }
+    
+    let results: [Float32] = output?.array ?? []
 
     // Process the results.
     let topNInferences = getTopN(results: results)
@@ -206,119 +143,5 @@ class ModelDataHandler {
       fatalError("Labels file named \(filename).\(fileExtension) cannot be read. Please add a " +
                    "valid labels file and try again.")
     }
-  }
-
-  /// Returns the RGB data representation of the given image buffer with the specified `byteCount`.
-  ///
-  /// - Parameters
-  ///   - buffer: The pixel buffer to convert to RGB data.
-  ///   - byteCount: The expected byte count for the RGB data calculated using the values that the
-  ///       model was trained on: `batchSize * imageWidth * imageHeight * componentsCount`.
-  ///   - isModelQuantized: Whether the model is quantized (i.e. fixed point values rather than
-  ///       floating point values).
-  /// - Returns: The RGB data representation of the image buffer or `nil` if the buffer could not be
-  ///     converted.
-  private func rgbDataFromBuffer(
-    _ buffer: CVPixelBuffer,
-    byteCount: Int,
-    isModelQuantized: Bool
-  ) -> Data? {
-    CVPixelBufferLockBaseAddress(buffer, .readOnly)
-    defer {
-      CVPixelBufferUnlockBaseAddress(buffer, .readOnly)
-    }
-    guard let sourceData = CVPixelBufferGetBaseAddress(buffer) else {
-      return nil
-    }
-    
-    let width = CVPixelBufferGetWidth(buffer)
-    let height = CVPixelBufferGetHeight(buffer)
-    let sourceBytesPerRow = CVPixelBufferGetBytesPerRow(buffer)
-    let destinationChannelCount = 3
-    let destinationBytesPerRow = destinationChannelCount * width
-    
-    var sourceBuffer = vImage_Buffer(data: sourceData,
-                                     height: vImagePixelCount(height),
-                                     width: vImagePixelCount(width),
-                                     rowBytes: sourceBytesPerRow)
-    
-    guard let destinationData = malloc(height * destinationBytesPerRow) else {
-      print("Error: out of memory")
-      return nil
-    }
-    
-    defer {
-        free(destinationData)
-    }
-
-    var destinationBuffer = vImage_Buffer(data: destinationData,
-                                          height: vImagePixelCount(height),
-                                          width: vImagePixelCount(width),
-                                          rowBytes: destinationBytesPerRow)
-
-    let pixelBufferFormat = CVPixelBufferGetPixelFormatType(buffer)
-
-    switch (pixelBufferFormat) {
-    case kCVPixelFormatType_32BGRA:
-        vImageConvert_BGRA8888toRGB888(&sourceBuffer, &destinationBuffer, UInt32(kvImageNoFlags))
-    case kCVPixelFormatType_32ARGB:
-        vImageConvert_ARGB8888toRGB888(&sourceBuffer, &destinationBuffer, UInt32(kvImageNoFlags))
-    case kCVPixelFormatType_32RGBA:
-        vImageConvert_RGBA8888toRGB888(&sourceBuffer, &destinationBuffer, UInt32(kvImageNoFlags))
-    default:
-        // Unknown pixel format.
-        return nil
-    }
-
-    let byteData = Data(bytes: destinationBuffer.data, count: destinationBuffer.rowBytes * height)
-    if isModelQuantized {
-        return byteData
-    }
-
-    // Not quantized, convert to floats
-    let bytes = Array<UInt8>(unsafeData: byteData)!
-    var floats = [Float]()
-    for i in 0..<bytes.count {
-        floats.append(Float(bytes[i]) / 255.0)
-    }
-    return Data(copyingBufferOf: floats)
-  }
-}
-
-// MARK: - Extensions
-
-extension Data {
-  /// Creates a new buffer by copying the buffer pointer of the given array.
-  ///
-  /// - Warning: The given array's element type `T` must be trivial in that it can be copied bit
-  ///     for bit with no indirection or reference-counting operations; otherwise, reinterpreting
-  ///     data from the resulting buffer has undefined behavior.
-  /// - Parameter array: An array with elements of type `T`.
-  init<T>(copyingBufferOf array: [T]) {
-    self = array.withUnsafeBufferPointer(Data.init)
-  }
-}
-
-extension Array {
-  /// Creates a new array from the bytes of the given unsafe data.
-  ///
-  /// - Warning: The array's `Element` type must be trivial in that it can be copied bit for bit
-  ///     with no indirection or reference-counting operations; otherwise, copying the raw bytes in
-  ///     the `unsafeData`'s buffer to a new array returns an unsafe copy.
-  /// - Note: Returns `nil` if `unsafeData.count` is not a multiple of
-  ///     `MemoryLayout<Element>.stride`.
-  /// - Parameter unsafeData: The data containing the bytes to turn into an array.
-  init?(unsafeData: Data) {
-    guard unsafeData.count % MemoryLayout<Element>.stride == 0 else { return nil }
-    #if swift(>=5.0)
-    self = unsafeData.withUnsafeBytes { .init($0.bindMemory(to: Element.self)) }
-    #else
-    self = unsafeData.withUnsafeBytes {
-      .init(UnsafeBufferPointer<Element>(
-        start: $0,
-        count: unsafeData.count / MemoryLayout<Element>.stride
-      ))
-    }
-    #endif  // swift(>=5.0)
   }
 }
