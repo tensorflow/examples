88748850f2d13dc10c735efd03124bacf647cd20
Khanh LeViet <khanhlvg@google.com>
2021-11-10 09:17:04 +0900

    Fix feedback comments.

--- lite/examples/pose_estimation/android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/tracker/AbstractTracker.kt
+++ lite/examples/pose_estimation/android/app/src/main/java/org/tensorflow/lite/examples/poseestimation/tracker/AbstractTracker.kt
@@ -1,3 +1,19 @@
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================
+*/
+
 package org.tensorflow.lite.examples.poseestimation.tracker
 
 import org.tensorflow.lite.examples.poseestimation.data.Person
@@ -55,8 +71,9 @@ abstract class AbstractTracker(val config: TrackerConfig) {
      * @param timestamp The current timestamp in microseconds.
      */
     private fun assignTrack(persons: List<Person>, simMatrix: List<List<Float>>, timestamp: Long) {
-        assert(simMatrix.size == persons.size && simMatrix[0].size == tracks.size) {
-            throw IllegalArgumentException()
+        if ((simMatrix.size != persons.size) != (simMatrix[0].size != tracks.size)) {
+            throw IllegalArgumentException(
+                "Size of person array and similarity matrix does not match.")
         }
 
         val unmatchedTrackIndices = MutableList(tracks.size) { it }
