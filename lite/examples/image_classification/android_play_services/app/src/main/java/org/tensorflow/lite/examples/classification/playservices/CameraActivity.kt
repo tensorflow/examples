011fb3e6c3f0d95812f945d3627ae13b4b56ebbe
A. Unique TensorFlower <gardener@tensorflow.org>
2022-02-16 09:33:46 -0800

    Sample app use tflite in play services for image classification.
    PiperOrigin-RevId: 429068462

---
+++ lite/examples/image_classification/android_play_services/app/src/main/java/org/tensorflow/lite/examples/classification/playservices/CameraActivity.kt
@@ -0,0 +1,266 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tensorflow.lite.examples.classification.playservices
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tflite.java.TfLite
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.random.Random
+import org.tensorflow.lite.examples.classification.playservices.databinding.ActivityCameraBinding
+
+/** Activity that displays the camera and performs object detection on the incoming frames */
+class CameraActivity : AppCompatActivity() {
+
+  private lateinit var activityCameraBinding: ActivityCameraBinding
+
+  private lateinit var bitmapBuffer: Bitmap
+
+  private val executor = Executors.newSingleThreadExecutor()
+  private val permissions = listOf(Manifest.permission.CAMERA)
+  private val permissionsRequestCode = Random.nextInt(0, 10000)
+
+  private var lensFacing: Int = CameraSelector.LENS_FACING_BACK
+  private val isFrontFacing
+    get() = lensFacing == CameraSelector.LENS_FACING_FRONT
+
+  private var pauseAnalysis = false
+  private var imageRotationDegrees: Int = 0
+
+  // Initialize TFLite once. Must be called before creating the classifier
+  private val initializeTask: Task<Void> by lazy { TfLite.initialize(this) }
+  private var classifier: ImageClassificationHelper? = null
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    activityCameraBinding = ActivityCameraBinding.inflate(layoutInflater)
+    setContentView(activityCameraBinding.root)
+
+    // Initialize TFLite asynchronously
+    initializeTask
+      .addOnSuccessListener {
+        Log.d(TAG, "TFLite in Play Services initialized successfully.")
+        classifier = ImageClassificationHelper(this)
+      }
+      .addOnFailureListener { e -> Log.e(TAG, "TFLite in Play Services failed to initialize.", e) }
+
+    activityCameraBinding.cameraCaptureButton.setOnClickListener {
+      // Disable all camera controls
+      it.isEnabled = false
+      if (pauseAnalysis) {
+        // If image analysis is in paused state, resume it
+        pauseAnalysis = false
+        activityCameraBinding.imagePredicted.visibility = View.GONE
+      } else {
+        // Otherwise, pause image analysis and freeze image
+        pauseAnalysis = true
+        val matrix =
+          Matrix().apply {
+            postRotate(imageRotationDegrees.toFloat())
+            if (isFrontFacing) postScale(-1f, 1f)
+          }
+        val uprightImage =
+          Bitmap.createBitmap(
+            bitmapBuffer,
+            0,
+            0,
+            bitmapBuffer.width,
+            bitmapBuffer.height,
+            matrix,
+            true
+          )
+        activityCameraBinding.imagePredicted.setImageBitmap(uprightImage)
+        activityCameraBinding.imagePredicted.visibility = View.VISIBLE
+      }
+
+      // Re-enable camera controls
+      it.isEnabled = true
+    }
+  }
+
+  override fun onDestroy() {
+    // Terminate all outstanding analyzing jobs (if there is any).
+    executor.apply {
+      shutdown()
+      awaitTermination(1000, TimeUnit.MILLISECONDS)
+    }
+    // Release TFLite resources
+    classifier?.close()
+    super.onDestroy()
+  }
+
+  /** Declare and bind preview and analysis use cases */
+  @SuppressLint("UnsafeExperimentalUsageError")
+  private fun bindCameraUseCases() =
+    activityCameraBinding.viewFinder.post {
+      val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+      cameraProviderFuture.addListener(
+        {
+          // Camera provider is now guaranteed to be available
+          val cameraProvider = cameraProviderFuture.get()
+
+          // Set up the view finder use case to display camera preview
+          val preview =
+            Preview.Builder()
+              .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+              .setTargetRotation(activityCameraBinding.viewFinder.display.rotation)
+              .build()
+
+          // Set up the image analysis use case which will process frames in real time
+          val imageAnalysis =
+            ImageAnalysis.Builder()
+              .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+              .setTargetRotation(activityCameraBinding.viewFinder.display.rotation)
+              .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+              .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+              .build()
+
+          var frameCounter = 0
+          var lastFpsTimestamp = System.currentTimeMillis()
+
+          imageAnalysis.setAnalyzer(
+            executor,
+            ImageAnalysis.Analyzer { image ->
+              if (!::bitmapBuffer.isInitialized) {
+                // The image rotation and RGB image buffer are initialized only once
+                // the analyzer has started running
+                imageRotationDegrees = image.imageInfo.rotationDegrees
+                bitmapBuffer =
+                  Bitmap.createBitmap(image.width, image.height, Bitmap.Config.ARGB_8888)
+              }
+
+              // Early exit: image analysis is in paused state, or TFLite is not initialized
+              if (pauseAnalysis || classifier == null) {
+                image.close()
+                return@Analyzer
+              }
+
+              // Copy out RGB bits to our shared buffer
+              image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
+
+              // Perform the image classification for the current frame
+              val recognitions = classifier?.classify(bitmapBuffer, imageRotationDegrees)
+
+              reportRecognition(recognitions)
+
+              // Compute the FPS of the entire pipeline
+              val frameCount = 10
+              if (++frameCounter % frameCount == 0) {
+                frameCounter = 0
+                val now = System.currentTimeMillis()
+                val delta = now - lastFpsTimestamp
+                val fps = 1000 * frameCount.toFloat() / delta
+                Log.d(TAG, "FPS: ${"%.02f".format(fps)}")
+                lastFpsTimestamp = now
+              }
+            }
+          )
+
+          // Create a new camera selector each time, enforcing lens facing
+          val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+          // Apply declared configs to CameraX using the same lifecycle owner
+          cameraProvider.unbindAll()
+          cameraProvider.bindToLifecycle(
+            this as LifecycleOwner,
+            cameraSelector,
+            preview,
+            imageAnalysis
+          )
+
+          // Use the camera object to link our preview use case with the view
+          preview.setSurfaceProvider(activityCameraBinding.viewFinder.surfaceProvider)
+        },
+        ContextCompat.getMainExecutor(this)
+      )
+    }
+
+  /** Displays recognition results on screen. */
+  private fun reportRecognition(
+    recognitions: List<ImageClassificationHelper.Recognition>?,
+  ) =
+    activityCameraBinding.viewFinder.post {
+
+      // Early exit: if recognition is null, or there are not enough recognition results.
+      if (recognitions == null || recognitions.size < MAX_REPORT) {
+        activityCameraBinding.textPrediction.visibility = View.GONE
+        return@post
+      }
+
+      // Update the text and UI
+      activityCameraBinding.textPrediction.text =
+        recognitions.subList(0, MAX_REPORT).joinToString(separator = "\n") {
+          "${"%.2f".format(it.confidence)} ${it.title}"
+        }
+
+      // Make sure all UI elements are visible
+      activityCameraBinding.textPrediction.visibility = View.VISIBLE
+    }
+
+  override fun onResume() {
+    super.onResume()
+
+    // Request permissions each time the app resumes, since they can be revoked at any time
+    if (!hasPermissions(this)) {
+      ActivityCompat.requestPermissions(this, permissions.toTypedArray(), permissionsRequestCode)
+    } else {
+      bindCameraUseCases()
+    }
+  }
+
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<out String>,
+    grantResults: IntArray,
+  ) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    if (requestCode == permissionsRequestCode && hasPermissions(this)) {
+      bindCameraUseCases()
+    } else {
+      finish() // If we don't have the required permissions, we can't run
+    }
+  }
+
+  /** Convenience method used to check if all permissions required by this app are granted */
+  private fun hasPermissions(context: Context) =
+    permissions.all {
+      ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+  companion object {
+    private val TAG = CameraActivity::class.java.simpleName
+    private const val MAX_REPORT = 3
+  }
+}
---
+++ lite/examples/image_classification/android_play_services/app/src/main/java/org/tensorflow/lite/examples/classification/playservices/ImageClassificationHelper.kt
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tensorflow.lite.examples.classification.playservices
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.Log
+import android.util.Size
+import java.io.Closeable
+import java.util.PriorityQueue
+import kotlin.math.min
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.InterpreterApi
+import org.tensorflow.lite.InterpreterApi.Options.TfLiteRuntime
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.common.TensorProcessor
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp
+import org.tensorflow.lite.support.image.ops.Rot90Op
+import org.tensorflow.lite.support.label.TensorLabel
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+
+/** Helper class used to communicate between our app and the TF image classification model */
+class ImageClassificationHelper(context: Context) : Closeable {
+
+  /** Abstraction object that wraps a classification output in an easy to parse way */
+  data class Recognition(val id: String, val title: String, val confidence: Float)
+
+  private val preprocessNormalizeOp = NormalizeOp(IMAGE_MEAN, IMAGE_STD)
+  private val postprocessNormalizeOp = NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD)
+  private val labels by lazy { FileUtil.loadLabels(context, LABELS_PATH) }
+  private var tfInputBuffer = TensorImage(DataType.UINT8)
+  private var tfImageProcessor: ImageProcessor? = null
+
+  // Processor to apply post processing of the output probability
+  private val probabilityProcessor = TensorProcessor.Builder().add(postprocessNormalizeOp).build()
+
+  // Use TFLite in Play Services runtime by setting the option to FROM_SYSTEM_ONLY
+  private val interpreterInitializer = lazy {
+    val interpreterOption = InterpreterApi.Options().setRuntime(TfLiteRuntime.FROM_SYSTEM_ONLY)
+    InterpreterApi.create(FileUtil.loadMappedFile(context, MODEL_PATH), interpreterOption)
+  }
+  // Only use interpreter after initialization finished in CameraActivity
+  private val interpreter: InterpreterApi by interpreterInitializer
+  private val tfInputSize by lazy {
+    val inputIndex = 0
+    val inputShape = interpreter.getInputTensor(inputIndex).shape()
+    Size(inputShape[2], inputShape[1]) // Order of axis is: {1, height, width, 3}
+  }
+
+  // Output probability TensorBuffer
+  private val outputProbabilityBuffer: TensorBuffer by lazy {
+    val probabilityTensorIndex = 0
+    val probabilityShape =
+      interpreter.getOutputTensor(probabilityTensorIndex).shape() // {1, NUM_CLASSES}
+    val probabilityDataType = interpreter.getOutputTensor(probabilityTensorIndex).dataType()
+    TensorBuffer.createFixedSize(probabilityShape, probabilityDataType)
+  }
+
+  /** Classifies the input bitmapBuffer. */
+  fun classify(bitmapBuffer: Bitmap, imageRotationDegrees: Int): List<Recognition> {
+    // Loads the input bitmapBuffer
+    tfInputBuffer = loadImage(bitmapBuffer, imageRotationDegrees)
+    Log.d(TAG, "tensorSize: ${tfInputBuffer.width} x ${tfInputBuffer.height}")
+
+    // Runs the inference call
+    interpreter.run(tfInputBuffer.buffer, outputProbabilityBuffer.buffer.rewind())
+
+    // Gets the map of label and probability
+    val labeledProbability =
+      TensorLabel(labels, probabilityProcessor.process(outputProbabilityBuffer)).mapWithFloatValue
+
+    return getTopKProbability(labeledProbability)
+  }
+
+  /** Releases TFLite resources if initialized. */
+  override fun close() {
+    if (interpreterInitializer.isInitialized()) {
+      interpreter.close()
+    }
+  }
+
+  /** Loads input image, and applies preprocessing. */
+  private fun loadImage(bitmapBuffer: Bitmap, imageRotationDegrees: Int): TensorImage {
+    // Initializes preprocessor if null
+    return (tfImageProcessor
+        ?: run {
+          val cropSize = minOf(bitmapBuffer.width, bitmapBuffer.height)
+          ImageProcessor.Builder()
+            .add(ResizeWithCropOrPadOp(cropSize, cropSize))
+            .add(
+              ResizeOp(
+                tfInputSize.height,
+                tfInputSize.width,
+                ResizeOp.ResizeMethod.NEAREST_NEIGHBOR
+              )
+            )
+            .add(Rot90Op(-imageRotationDegrees / 90))
+            .add(preprocessNormalizeOp)
+            .build()
+            .also {
+              tfImageProcessor = it
+              Log.d(TAG, "tfImageProcessor initialized successfully. imageSize: $cropSize")
+            }
+        })
+      .process(tfInputBuffer.apply { load(bitmapBuffer) })
+  }
+
+  /** Gets the top-k results. */
+  private fun getTopKProbability(labelProb: Map<String, Float>): List<Recognition> {
+    // Sort the recognition by confidence from high to low.
+    val pq: PriorityQueue<Recognition> =
+      PriorityQueue(MAX_RESULTS, compareByDescending<Recognition> { it.confidence })
+    pq += labelProb.map { (label, prob) -> Recognition(label, label, prob) }
+    return List(min(MAX_RESULTS, pq.size)) { pq.poll()!! }
+  }
+
+  companion object {
+    private val TAG = ImageClassificationHelper::class.java.simpleName
+    // Returns the top MAX_RESULTS recognitions
+    private const val MAX_RESULTS = 10
+
+    // ClassifierFloatEfficientNet model
+    private const val MODEL_PATH = "efficientnet-lite0-fp32.tflite"
+    private const val LABELS_PATH = "labels_without_background.txt"
+    // Float model does not need dequantization in the post-processing. Setting mean and std as
+    // 0.0f and 1.0f, respectively, to bypass the normalization
+    private const val PROBABILITY_MEAN = 0.0f
+    private const val PROBABILITY_STD = 1.0f
+    private const val IMAGE_MEAN = 127.0f
+    private const val IMAGE_STD = 128.0f
+  }
+}
