2cda96e11f2b7edd7ccdb1617ab436dc1033ad0a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 00:32:55 +0000

    Adding tarfile member sanitization to extractall()

--- lite/examples/speech_commands/ml/download.py
+++ lite/examples/speech_commands/ml/download.py
@@ -34,6 +34,25 @@ if os.path.exists('data'):
 os.makedirs('data/train')
 
 with tarfile.open(ARCHIVE, 'r:gz') as tar:
-  tar.extractall(path='data/train')
+  def is_within_directory(directory, target):
+      
+      abs_directory = os.path.abspath(directory)
+      abs_target = os.path.abspath(target)
+  
+      prefix = os.path.commonprefix([abs_directory, abs_target])
+      
+      return prefix == abs_directory
+  
+  def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+  
+      for member in tar.getmembers():
+          member_path = os.path.join(path, member.name)
+          if not is_within_directory(path, member_path):
+              raise Exception("Attempted Path Traversal in Tar File")
+  
+      tar.extractall(path, members, numeric_owner=numeric_owner) 
+      
+  
+  safe_extract(tar, path="data/train")
 
 os.remove(ARCHIVE)
