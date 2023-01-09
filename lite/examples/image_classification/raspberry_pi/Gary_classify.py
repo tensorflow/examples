bc674e8f4473d3f0d9a8a20a7593f205ad8f31cf
bubba-coder <mercer.gary@gmail.com>
2023-01-09 21:13:38 +0000

    tensor flow

--- lite/examples/image_classification/raspberry_pi/Gary_classify.py
+++ lite/examples/image_classification/raspberry_pi/Gary_classify.py
@@ -17,10 +17,10 @@ import argparse
 import sys
 import time
 
-import cv2
 from tflite_support.task import core
 from tflite_support.task import processor
 from tflite_support.task import vision
+from PIL import Image
 
 # Visualization parameters
 _ROW_SIZE = 20  # pixels
@@ -32,7 +32,7 @@ _FPS_AVERAGE_FRAME_COUNT = 10
 
 
 def run(model: str, max_results: int, score_threshold: float, num_threads: int,
-        enable_edgetpu: bool, camera_id: int, width: int, height: int) -> None:
+        enable_edgetpu: bool, width: int, height: int) -> None:
   """Continuously run inference on images acquired from the camera.
 
   Args:
@@ -62,62 +62,40 @@ def run(model: str, max_results: int, score_threshold: float, num_threads: int,
   counter, fps = 0, 0
   start_time = time.time()
 
-  # Start capturing video input from the camera
-  cap = cv2.VideoCapture(camera_id)
-  cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-  cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+ 
+  # Create TensorImage from the RGB image
+ # rgb_image = Image.open("test_data/fox.jpeg").convert('RGB').resize((width, height))
+ # tensor_image = vision.TensorImage.create_from_array(rgb_image)
+  
+ # test_file = "test_data/fox.jpeg"
+  test_file = "test_data/squirrel.jpeg"
 
-  # Continuously capture images from the camera and run inference
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      sys.exit(
-          'ERROR: Unable to read from webcam. Please verify your webcam settings.'
-      )
+  tensor_image = vision.TensorImage.create_from_file(test_file)
 
-
+  # List classification results
+  categories = classifier.classify(tensor_image)
 
 
-    counter += 1
-    image = cv2.flip(image, 1)
-
-    # Convert the image from BGR to RGB as required by the TFLite model.
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    # Create TensorImage from the RGB image
-    tensor_image = vision.TensorImage.create_from_array(rgb_image)
-    # List classification results
-    categories = classifier.classify(tensor_image)
-
-    # Show classification results on the image
-    for idx, category in enumerate(categories.classifications[0].categories):
+  # Show classification results on the image
+  print(test_file+" : RESULTS")
+  for idx, category in enumerate(categories.classifications[0].categories):
       category_name = category.category_name
       score = round(category.score, 2)
       result_text = category_name + ' (' + str(score) + ')'
-      text_location = (_LEFT_MARGIN, (idx + 2) * _ROW_SIZE)
-      cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                  _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
+      print(" : "+result_text )
 
-    # Calculate the FPS
-    if counter % _FPS_AVERAGE_FRAME_COUNT == 0:
+  # Calculate the FPS
+  if counter % _FPS_AVERAGE_FRAME_COUNT == 0:
       end_time = time.time()
       fps = _FPS_AVERAGE_FRAME_COUNT / (end_time - start_time)
       start_time = time.time()
 
-    # Show the FPS
-    fps_text = 'FPS = ' + str(int(fps))
-    text_location = (_LEFT_MARGIN, _ROW_SIZE)
-    cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
+  # Show the FPS
+  fps_text = 'FPS = ' + str(int(fps))
+  text_location = (_LEFT_MARGIN, _ROW_SIZE)
+  
 
-    # Stop the program if the ESC key is pressed.
-    if cv2.waitKey(1) == 27:
-      break
-    cv2.imshow('image_classification', image)
-
-  cap.release()
-  cv2.destroyAllWindows()
-
+ 
 
 def main():
   parser = argparse.ArgumentParser(
@@ -149,10 +127,10 @@ def main():
       action='store_true',
       required=False,
       default=False)
+#  parser.add_argument(
+#     '--cameraId', help='Id of camera.', required=False, default=0)
   parser.add_argument(
-      '--cameraId', help='Id of camera.', required=False, default=0)
-  parser.add_argument(
-      '--frameWidth',
+      '--frameWidth',  
       help='Width of frame to capture from camera.',
       required=False,
       default=640)
@@ -165,7 +143,7 @@ def main():
 
   run(args.model, int(args.maxResults),
       args.scoreThreshold, int(args.numThreads), bool(args.enableEdgeTPU),
-      int(args.cameraId), args.frameWidth, args.frameHeight)
+      args.frameWidth, args.frameHeight)
 
 
 if __name__ == '__main__':
