b08a3fd6523e91ebe17015e360501a0dc54267fe
Fergus Henderson <fergus@google.com>
2024-12-16 03:04:55 -0800

    Some clean ups for the 3P C++ API's Gradle / CMake integration.
    In particular:
    
    - Use `list(PREPEND ...)` rather than `set(...)` to prepend to `CMAKE_MODULE_PATH`.
    
    - Clarify the distinction between `tflite_cc_DIR` and `tflite_cc_INCLUDE_DIR`,
      and use the former rather than the latter for the call to `add_subdirectory`.
    
    - Define `tflite_cc_api_DIR` in the same place where `tflite_cc_INCLUDE_DIR`
      is defined, i.e. in `Findtflite_cc_api.cmake`, rather than in a different file.
    
    - In `Findtflite_cc_api.cmake`, verify that the appropriate files exist in
      the specified directory, and report error(s) if they don't.
      (If CMake is invoked via Gradle, then the Gradle file
      should already ensure that those files exist there, by unpacking the
      contents of the AAR file and copying the `cc_sdk` directory there;
      but having explicit checks here means we'll fail
      earlier, and hopefully with a better error message, if CMake is invoked in some
      other way or if future changes break things.)
    
    PiperOrigin-RevId: 706640601

--- lite/examples/native_api/android_play_services/cc_api/app/src/main/cpp/Modules/Findtflite_cc_api.cmake
+++ lite/examples/native_api/android_play_services/cc_api/app/src/main/cpp/Modules/Findtflite_cc_api.cmake
@@ -1,5 +1,25 @@
 # This CMake code fragment determines the location of the tflite_cc_api package.
-set(tflite_cc_api_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
+
+# tflite_cc_api_DIR is the directory containing the CMakeLists.txt file
+# for building the TFLite-in-Play-services C++ API client SDK source files.
+if (NOT EXISTS "${CMAKE_SOURCE_DIR}/tflite_cc_sdk/CMakeLists.txt")
+        message(SEND_ERROR "CMakeLists.txt not found in ${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
+elseif (NOT EXISTS "${CMAKE_SOURCE_DIR}/tflite_cc_sdk/tensorflow/lite/abi/cc/interpreter.cc")
+        message(SEND_ERROR "tensorflow/lite/abi/cc/interpreter.cc not found in ${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
+else()
+        set(tflite_cc_api_DIR "${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
+endif()
+
+# tflite_cc_api_INCLUDE_DIR is the include directory containing the header
+# files for the TFLite-in-Play-services C++ API.
+if (NOT EXISTS "${CMAKE_SOURCE_DIR}/tflite_cc_sdk/tensorflow/lite/interpreter.h")
+        message(SEND_ERROR "tensorflow/lite/interpreter.h not found in ${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
+elseif (NOT EXISTS "${CMAKE_SOURCE_DIR}/tflite_cc_sdk/tensorflow/lite/abi/cc/interpreter.h")
+        message(SEND_ERROR "tensorflow/lite/abi/cc/interpreter.h not found in ${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
+else()
+        set(tflite_cc_api_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(tflite_cc_api DEFAULT_MSG
-                                  tflite_cc_api_INCLUDE_DIR)
+                                  tflite_cc_api_DIR tflite_cc_api_INCLUDE_DIR)
