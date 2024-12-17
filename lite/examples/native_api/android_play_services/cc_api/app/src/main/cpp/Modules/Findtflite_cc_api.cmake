652ee34ff046946c36b8aed5d97ecebab0699f7e
Fergus Henderson <fergus@google.com>
2024-12-17 06:36:38 -0800

    Some more clean ups for the 3P C++ API's Gradle / CMake integration.
    In particular:
    
      - Copy the extracted SDK sources into $projectDir/build rather than into $projectDir/src/...
      - Remove the unnecessary `addTfLiteNativeDeps` task, which is empty apart from its dependencies.
    
    PiperOrigin-RevId: 707074726

--- lite/examples/native_api/android_play_services/cc_api/app/src/main/cpp/Modules/Findtflite_cc_api.cmake
+++ lite/examples/native_api/android_play_services/cc_api/app/src/main/cpp/Modules/Findtflite_cc_api.cmake
@@ -1,23 +1,25 @@
 # This CMake code fragment determines the location of the tflite_cc_api package.
 
+set(gradle_src_root "${CMAKE_SOURCE_DIR}/../../..")
+
 # tflite_cc_api_DIR is the directory containing the CMakeLists.txt file
 # for building the TFLite-in-Play-services C++ API client SDK source files.
-if (NOT EXISTS "${CMAKE_SOURCE_DIR}/tflite_cc_sdk/CMakeLists.txt")
-        message(SEND_ERROR "CMakeLists.txt not found in ${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
-elseif (NOT EXISTS "${CMAKE_SOURCE_DIR}/tflite_cc_sdk/tensorflow/lite/abi/cc/interpreter.cc")
-        message(SEND_ERROR "tensorflow/lite/abi/cc/interpreter.cc not found in ${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
+if (NOT EXISTS "${gradle_src_root}/build/tflite_cc_sdk/CMakeLists.txt")
+        message(SEND_ERROR "CMakeLists.txt not found in ${gradle_src_root}/build/tflite_cc_sdk")
+elseif (NOT EXISTS "${gradle_src_root}/build/tflite_cc_sdk/tensorflow/lite/abi/cc/interpreter.cc")
+        message(SEND_ERROR "tensorflow/lite/abi/cc/interpreter.cc not found in ${gradle_src_root}/build/tflite_cc_sdk")
 else()
-        set(tflite_cc_api_DIR "${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
+        set(tflite_cc_api_DIR "${gradle_src_root}/build/tflite_cc_sdk")
 endif()
 
 # tflite_cc_api_INCLUDE_DIR is the include directory containing the header
 # files for the TFLite-in-Play-services C++ API.
-if (NOT EXISTS "${CMAKE_SOURCE_DIR}/tflite_cc_sdk/tensorflow/lite/interpreter.h")
-        message(SEND_ERROR "tensorflow/lite/interpreter.h not found in ${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
-elseif (NOT EXISTS "${CMAKE_SOURCE_DIR}/tflite_cc_sdk/tensorflow/lite/abi/cc/interpreter.h")
-        message(SEND_ERROR "tensorflow/lite/abi/cc/interpreter.h not found in ${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
+if (NOT EXISTS "${gradle_src_root}/build/tflite_cc_sdk/tensorflow/lite/interpreter.h")
+        message(SEND_ERROR "tensorflow/lite/interpreter.h not found in ${gradle_src_root}/build/tflite_cc_sdk")
+elseif (NOT EXISTS "${gradle_src_root}/build/tflite_cc_sdk/tensorflow/lite/abi/cc/interpreter.h")
+        message(SEND_ERROR "tensorflow/lite/abi/cc/interpreter.h not found in ${gradle_src_root}/build/tflite_cc_sdk")
 else()
-        set(tflite_cc_api_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/tflite_cc_sdk")
+        set(tflite_cc_api_INCLUDE_DIR "${gradle_src_root}/tflite_cc_sdk")
 endif()
 
 include(FindPackageHandleStandardArgs)
